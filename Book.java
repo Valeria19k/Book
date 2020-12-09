4f0bf586d2e0eb55ece56dd07d64a3422586b944
KimVal <71520455+Valeria19k@users.noreply.github.com>
2020-12-09 16:46:08 +0300

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Book {
+    private int pages;
+    private int year;
+    private String title;
+    private String author;
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
